936ba2d0b9be72919fa892d135b0487d3f4b00e1
Katerina Zabrovskaya <>
2019-12-19 16:30:23 +0300

    trying deployment v0.2

--- back/app.js
+++ back/app.js
@@ -60,11 +60,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Heroku deployment
+if (process.env.NODE_ENV === 'PRODUCTION') {
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+}
 // Allows you to use PUT, DELETE with forms.
 app.use(
   methodOverride(function (req, res) {
