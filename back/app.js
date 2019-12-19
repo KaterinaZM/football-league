9143f52f26cc338a001b3625ee8351c4de796717
Katerina Zabrovskaya <>
2019-12-19 16:13:07 +0300

    trying to deploy v0.1

--- back/app.js
+++ back/app.js
@@ -59,6 +59,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Heroku deployment
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // Allows you to use PUT, DELETE with forms.
 app.use(
   methodOverride(function (req, res) {
