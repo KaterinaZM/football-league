7b38f9a8222095eb578f04a1e9231cbcd54d6015
Mikhail <mvafonin@yandex.ru>
2019-12-20 12:01:20 +0300

    Release 1.7.4

--- front/src/components/Profile/LeftSide/Menu/Menu.jsx
+++ front/src/components/Profile/LeftSide/Menu/Menu.jsx
@@ -1,44 +1,87 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Logout from '../Logout/Logout';
 import "./Menu.css";
 
 export default class Menu extends Component {
   render() {
     return (
-      <div className="menu">
-        <h3 className="menu__title">Menu</h3>
+      <div className="profile-menu">
+        <h3 className="profile-menu__title">Menu</h3>
 
         <nav>
-          <ul className="menu__list">
-            <li className="menu__item">
-              <Link className="menu__link" to="/profile">
+
+          {/* HAMBURGER MENU */}
+          <div className="menu-wrap">
+            <input type="checkbox" className="toggler" />>
+            <div className="hamburger"><div></div></div>
+            <div className="menu" >
+              <div id='ham-menu'>
+                <div className='ham-menu' >
+                <h3 className="profile-menu__title-hamburger">Menu</h3>
+                  <ul>
+                    <li>
+                      <Link className="profile-menu__link" to="/profile">
+                        Main
+                      </Link>
+                    </li>
+
+                    <li className="header__hamburger-menu-item">
+                      <Link className="profile-menu__link" to="/profile/leagues">
+                        Browse Leagues
+                      </Link>
+                    </li>
+
+                    <li className="header__hamburger-menu-item">
+                      <Link className="profile-menu__link" to="/profile/playgrounds">
+                        Playgrounds
+                      </Link>
+                    </li>
+
+                    <li className="header__hamburger-menu-item">
+                      <Link className="profile-menu__link" to="/profile/calendar">
+                        Create Event
+                      </Link>
+                    </li>
+
+                    <li className="header__hamburger-menu-item">
+                      <Link className="profile-menu__link" to="/profile/achievements">
+                        <Logout {...this.props} />
+                      </Link>
+                    </li> 
+
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <ul className="profile-menu__list">
+            
+            <li className="profile-menu__item">
+              <Link className="profile-menu__link" to="/profile">
                 Main
               </Link>
             </li>
 
-            <li className="menu__item">
-              <Link className="menu__link" to="/profile/leagues">
+            <li className="profile-menu__item">
+              <Link className="profile-menu__link" to="/profile/leagues">
                 Browse Leagues
               </Link>
             </li>
 
-            <li className="menu__item">
-              <Link className="menu__link" to="/profile/playgrounds">
+            <li className="profile-menu__item">
+              <Link className="profile-menu__link" to="/profile/playgrounds">
                 Playgrounds
               </Link>
             </li>
 
-            <li className="menu__item">
-              <Link className="menu__link" to="/profile/calendar">
-                My Events
+            <li className="profile-menu__item">
+              <Link className="profile-menu__link" to="/profile/calendar">
+              Create Event
               </Link>
             </li>
 
-            <li className="menu__item">
-              <Link className="menu__link" to="/profile/achievements">
-                Achievements
-              </Link>
-            </li>
           </ul>
         </nav>
       </div>
